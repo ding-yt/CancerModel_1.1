04fb82d2f9e1dfb8c58429b207fc0afe72c562f5
Yuantong Ding <yd44@duke.edu>
2013-11-07 15:30:45 -0500

    Initial Commit

---
+++ CancerModel_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CancerModel_1
+//
+//  Created by Yuantong Ding on 11/7/13.
+//  Copyright (c) 2013 Yuantong Ding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
